5f1629df98a4802f354e837c1137bd4f28f76563
Carlos Andreu <carlos.andreu@upr.edu>
2011-10-22 18:48:52 -0400

    controllers and layout

---
+++ app/controllers/candidatos_controller.rb
@@ -0,0 +1,23 @@
+class CandidatosController < ApplicationController
+  def all
+  end
+
+  def agustinirizarry
+  end
+
+  def cecilioortiz
+  end
+
+  def douglassantos
+  end
+
+  def hectorhuyke
+  end
+
+  def jorgeshmidt
+  end
+
+  def riverasantos
+  end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  
+  def index
+  end
+  
+  def about
+  end
+
+  def contact
+  end
+
+end
