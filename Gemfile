798f526f2930d5f13bf79f8e84c136050c4063fc
Carlos Andreu <carlos.andreu@upr.edu>
2011-10-22 18:16:19 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+gem 'rack', '>=1.3.3'
+gem 'rake', '>=0.9.2'
+gem 'annotate'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :test, :development do
+  # Pretty printed test output
+  gem 'turn', :require => false
+  gem 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RectorRum::Application.config.secret_token = '3f740feb662ac9417034c7e2338f11de02663e8067e55d5fb34051a73636904d838f6bb4372742863c1df487bfcbde610c11231ae9b80642cb5f7e7e69f7f41f'
