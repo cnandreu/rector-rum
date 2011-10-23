0ebfe46ed9bc1ef023bc4f57596e6e28e6e637bf
Carlos Andreu <carlos.andreu@upr.edu>
2011-10-23 17:53:21 -0400

    new pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,24 @@
 module ApplicationHelper
-  
+
   def title
     base_title = "Rector RUM 2011"
     @title.nil? ? base_title : "#{base_title} | #{@title}"
   end
-  
+
   def navigation(page_name, path)
     content_tag :li, link_to(page_name, path) , :class => check_active(page_name) 
   end
-  
+
   def check_active(page_name)
     (!@title.nil? && (@title.include? page_name) ) ? "active" : ""
   end
-  
-  def link_candidate(name,path)
-    link_to(image_tag(asset_path "128x150/#{name}.jpg"), path)
+
+  def link_candidate(name,path,size)
+    link_to(image_tag(asset_path "#{size}/#{name}.jpg"), path)
   end
-  
+
+  def image_candidate(name,size)
+    image_tag(asset_path "#{size}/#{name}.jpg", :class=>"thumbnail")
+  end
+
 end
